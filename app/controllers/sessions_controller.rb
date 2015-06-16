3c2f0ee8e64b109a844c9d76375a0b892b4c20f9
Daley Meistrell <dstrell14@gmail.com>
2015-06-16 15:49:23 -0700

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  	 # Fetches the OAuth response info
+    @auth = request.env['omniauth.auth']
+  end
+
+  def destroy
+  end
+end
